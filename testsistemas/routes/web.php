6073c7e0d6a06e2549b08ca76274fa694313166b
christianvillar <christian_villar@uca.edu.ar>
2020-12-22 21:46:56 -0300

    vista inicio

--- testsistemas/routes/web.php
+++ testsistemas/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/peticion',function(){
+    return 'peticion ejecutada';
+
+});
+Route::get('/Inicio',function(){
+    return view('inicio');
+});
